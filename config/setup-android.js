7c20d976a0b309dfd5dbcc9dc0d5ed2ac21141b9
rachidox <rachid.el.habbari@gmail.com>
2018-11-06 18:38:55 -0500

    windows fixes

--- config/setup-android.js
+++ config/setup-android.js
@@ -14,7 +14,7 @@ const path = require('path');
 
 const ANDROID_SDK = './android-sdk';
 const ANDROID_SDK_ZIP_OUTPUT = 'sdk-tools.zip';
-const ANDROID_SDK_MANAGER = `${ANDROID_SDK}/tools/bin/sdkmanager`;
+const ANDROID_SDK_MANAGER = `${ANDROID_SDK}/tools/bin/sdkmanager${process.platform === 'win32' ? '.bat' : ''}`;
 const BUILD_TOOLS_VERSION = `28.0.3`;
 const PLATFORM_VERSION = `android-28`;
 const GRADLE_ZIP_OUTPUT = 'gradle.zip';
@@ -22,95 +22,95 @@ const GRADLE_VERSION = `4.10.2`;
 const GRADLE_EXTRACT_PATH = './android-sdk';
 const GRADLE_PATH = `${GRADLE_EXTRACT_PATH}/gradle-${GRADLE_VERSION}`
 
-module.exports.withAndroidEnv = function(command) {
-	prependPath(path.resolve(`${GRADLE_PATH}/bin`));
-	prependPath(path.resolve(`${ANDROID_SDK}/platform-tools`));
-	prependPath(path.resolve(`${ANDROID_SDK}/tools`));
-	utils.exec(`ANDROID_HOME=${path.resolve(ANDROID_SDK)} GRADLE_HOME=${path.resolve(GRADLE_PATH)} ${command}`);
+module.exports.withAndroidEnv = function (command) {
+  prependPath(path.resolve(`${GRADLE_PATH}/bin`));
+  prependPath(path.resolve(`${ANDROID_SDK}/platform-tools`));
+  prependPath(path.resolve(`${ANDROID_SDK}/tools`));
+  utils.exec(`ANDROID_HOME=${path.resolve(ANDROID_SDK)} GRADLE_HOME=${path.resolve(GRADLE_PATH)} ${command}`);
 }
 
-module.exports.cordova = function(command) {
-	const cordovaExectuable = './node_modules/.bin/cordova';
+module.exports.cordova = function (command) {
+  const cordovaExectuable = './node_modules/.bin/cordova';
 
-	if (!fs.existsSync(cordovaExectuable)) {
-		console.error('Cordova is not installed in the project, please: npm install cordova');
-		return;
-	}
+  if (!fs.existsSync(cordovaExectuable)) {
+    console.error('Cordova is not installed in the project, please: npm install cordova');
+    return;
+  }
 
-	this.withAndroidEnv(`${cordovaExectuable} ${command}`);
+  this.withAndroidEnv(`${cordovaExectuable} ${command}`);
 }
 
-module.exports.downloadDependencies = function() {
-	downloadSdkTools();
+module.exports.downloadDependencies = function () {
+  downloadSdkTools();
 }
 
 function downloadSdkTools() {
-	const downloadUrl = getSdkToolsUrl();
-	
-	if (downloadUrl && !fs.existsSync(ANDROID_SDK_MANAGER)) {
-		utils.downloadZip(
-			getSdkToolsUrl(),  
-			ANDROID_SDK, 
-			ANDROID_SDK_ZIP_OUTPUT,
-			() => onSdkToolsDownloaded()
-		);
-	} else {
-		onSdkToolsDownloaded();
-	}
+  const downloadUrl = getSdkToolsUrl();
+
+  if (downloadUrl && !fs.existsSync(ANDROID_SDK_MANAGER)) {
+    utils.downloadZip(
+      getSdkToolsUrl(),
+      ANDROID_SDK,
+      ANDROID_SDK_ZIP_OUTPUT,
+      () => onSdkToolsDownloaded()
+    );
+  } else {
+    onSdkToolsDownloaded();
+  }
 }
 
 function onSdkToolsDownloaded() {
-	downloadGradle();
+  downloadGradle();
 }
 
 function downloadGradle() {
-	const downloadUrl = getGradleUrl();
+  const downloadUrl = getGradleUrl();
 
-	if (downloadUrl && !fs.existsSync(GRADLE_PATH)) {
-		utils.downloadZip(
-			getGradleUrl(),
-			GRADLE_EXTRACT_PATH,
-			GRADLE_ZIP_OUTPUT,
-			() => onGradleDownloaded()
-		);
-	} else {
-		onGradleDownloaded();
-	}
+  if (downloadUrl && !fs.existsSync(GRADLE_PATH)) {
+    utils.downloadZip(
+      getGradleUrl(),
+      GRADLE_EXTRACT_PATH,
+      GRADLE_ZIP_OUTPUT,
+      () => onGradleDownloaded()
+    );
+  } else {
+    onGradleDownloaded();
+  }
 }
 
 function onGradleDownloaded() {
-	prependPath(path.resolve(`${GRADLE_PATH}/bin`));
-	fs.chmodSync(`${GRADLE_PATH}/bin/gradle`, 0o555);
-	installAndroidSdk()
+  prependPath(path.resolve(`${GRADLE_PATH}/bin`));
+  fs.chmodSync(`${GRADLE_PATH}/bin/gradle`, 0o555);
+  installAndroidSdk()
 }
 
 function getGradleUrl() {
-	return `https://downloads.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip`;
+  return `https://downloads.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip`;
 }
 
 function installAndroidSdk() {
-	console.log(`Installing the android sdk ${PLATFORM_VERSION}, and build-tools version ${BUILD_TOOLS_VERSION}...`);
-	fs.chmodSync(ANDROID_SDK_MANAGER, 0o555);
-	utils.exec(`yes | ${ANDROID_SDK_MANAGER} "build-tools;${BUILD_TOOLS_VERSION}" "platforms;${PLATFORM_VERSION}"`)
+  console.log(`Installing the android sdk ${PLATFORM_VERSION}, and build-tools version ${BUILD_TOOLS_VERSION}...`);
+  fs.chmodSync(ANDROID_SDK_MANAGER, 0o555);
+  utils.exec(`yes | ${path.resolve(ANDROID_SDK_MANAGER)} "build-tools;${BUILD_TOOLS_VERSION}" "platforms;${PLATFORM_VERSION}"`)
 }
 
 function getSdkToolsUrl() {
-	// Mapping of the platform 
-	let mapping = {
-		'win32': 'windows',
-		'darwin': 'darwin',
-		'linux': 'linux'
-	}
-	
-	if (!mapping[process.platform]) {
-		console.error(`Your platform ${process.platform} is not supported by the android setup script.`);
-		return;
-	}
+  // Mapping of the platform 
+  let mapping = {
+    'win32': 'windows',
+    'darwin': 'darwin',
+    'linux': 'linux'
+  }
 
-	let sdkZip = `sdk-tools-${mapping[process.platform]}-4333796.zip`;
-	return `https://dl.google.com/android/repository/${sdkZip}`;
+  if (!mapping[process.platform]) {
+    console.error(`Your platform ${process.platform} is not supported by the android setup script.`);
+    return;
+  }
+
+  let sdkZip = `sdk-tools-${mapping[process.platform]}-4333796.zip`;
+  return `https://dl.google.com/android/repository/${sdkZip}`;
 }
 
 require('make-runnable/custom')({
-	printOutputFrame: false
-})
+  printOutputFrame: false
+});
--- config/utils.js
+++ config/utils.js
@@ -1,105 +1,102 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports.downloadZip = function(uri, dirname, outFile, cb) {
-	mkdir(dirname);
-	
-	let fileName = path.resolve(dirname, outFile);
+module.exports.downloadZip = function (uri, dirname, outFile, cb) {
+  mkdir(dirname);
 
-	download(uri, fileName, () => {
-		unzip(dirname, fileName, cb);
-	});
+  let fileName = path.resolve(dirname, outFile);
+
+  download(uri, fileName, () => {
+    unzip(dirname, fileName, cb);
+  });
 };
 
-module.exports.exec = function(command) {
-	const { execSync } = require('child_process');
-	
-	try {
-		execSync(command, {stdio: [0, 1, 2]});
-	} catch(err) {
-		console.error(err.toString());
-	}
+module.exports.exec = function (command) {
+  const { execSync } = require('child_process');
+
+  try {
+    execSync(command, { stdio: [0, 1, 2] });
+  } catch (err) {
+    console.error(err.toString());
+  }
 }
 
 function unzip(dirname, fileName, cb) {
-	if (fs.existsSync(fileName)) {
-		console.log(`unzipping ${fileName}...`);
-		const admZip = require('adm-zip');
-		let zip = new admZip(fileName);
-		zip.extractAllTo(dirname);
-		console.log(`unzipping ${fileName} completed.`);
-	}
-		
-	if (cb) cb();
+  if (fs.existsSync(fileName)) {
+    console.log(`unzipping ${fileName}...`);
+    const admZip = require('adm-zip');
+    let zip = new admZip(fileName);
+    zip.extractAllTo(dirname);
+    console.log(`unzipping ${fileName} completed.`);
+  }
+
+  if (cb) cb();
 }
 
-function mkdir(dir){
-	if (fs.existsSync(dir)){
-		return;
-	}
+function mkdir(dir) {
+  if (fs.existsSync(dir)) {
+    return;
+  }
 
-	try{
-		fs.mkdirSync(dir)
-	} catch(err){
-		if(err.code == 'ENOENT'){
-			mkdir(path.dirname(dir)) //create parent dir
-			mkdir(dir) //create dir
-		}
-	}
+  try {
+    fs.mkdirSync(dir)
+  } catch (err) {
+    if (err.code == 'ENOENT') {
+      mkdir(path.dirname(dir)) //create parent dir
+      mkdir(dir) //create dir
+    }
+  }
 }
 
 function download(url, dest, callback) {
-	if (fs.existsSync(dest)) {
-		console.log(`File exists ${dest}, skipping download`);
-		callback();
-		return;
-	}
+  if (fs.existsSync(dest)) {
+    console.log(`File exists ${dest}, skipping download`);
+    callback();
+    return;
+  }
 
-	console.log(`Downloading ${url}...`);
+  console.log(`Downloading ${url}...`);
 
-	const lib = url.startsWith('https') ? require('https') : require('http');
-	
-	let file = fs.createWriteStream(dest);
-	
-	const sigIntListener = () => {
-		console.warn(`Caught interrupt signal while downloading dependencies, cleaning up`);
-		onDownloadComplete(true);
+  const lib = url.startsWith('https') ? require('https') : require('http');
+
+  let file = fs.createWriteStream(dest);
+
+  const sigIntListener = () => {
+    console.warn(`Caught interrupt signal while downloading dependencies, cleaning up`);
+    onDownloadComplete(true);
     process.exit();
-	};
+  };
 
-	process.on('SIGINT', sigIntListener);
+  process.on('SIGINT', sigIntListener);
 
-	const onDownloadComplete = (isError) => {
-		if (isError) {
-			console.log(`Deleting ${file.path}...`);
-			file.close(callback);
-			fs.unlinkSync(file.path);	
-		} else {
-			file.close(callback);
-		}
+  const onDownloadComplete = (isError) => {
+    if (isError) {
+      console.log(`Deleting ${file.path}...`);
+      file.close(callback);
+      fs.unlinkSync(file.path);
+    } else {
+      file.close(callback);
+    }
 
-		process.removeAllListeners('SIGINT', sigIntListener)
-	};
+    process.removeAllListeners('SIGINT', sigIntListener)
+  };
 
-	lib.get(url, response => {
-		const { statusCode } = response;
-		
-		if (statusCode !== 200) {
-			console.error(`Error while downloading ${url}, Status Code: ${statusCode}`);
-			onDownloadComplete(true);
-		} else {
-			response.pipe(file);
-		}
-	})
-	.on('end', err => {
-		onDownloadComplete(err ? true : false);
-	});
+  lib
+    .get(url, response => {
+      const { statusCode } = response;
 
-	
+      if (statusCode !== 200) {
+        console.error(`Error while downloading ${url}, Status Code: ${statusCode}`);
+        onDownloadComplete(true);
+      } else {
+        response.pipe(file);
+      }
+    })
+    .on('end', err => {
+      onDownloadComplete(err ? true : false);
+    });
 
-	const { finished } = require('stream');
-	finished(file, err => {
-		sigIntListener
-		onDownloadComplete(err ? true : false, callback);
-	});
+  file.on('finish', () => {
+    onDownloadComplete(false, callback);
+  });
 }
